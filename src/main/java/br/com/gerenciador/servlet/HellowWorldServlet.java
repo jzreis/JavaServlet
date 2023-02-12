8489a61d538c94cd7d30aacc70acc0508bd3878f
jzreiis <jz@godzilla.localdomain>
2023-02-11 22:02:21 -0300

    update

---
+++ src/main/java/br/com/gerenciador/servlet/HellowWorldServlet.java
@@ -0,0 +1,28 @@
+package br.com.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/home")
+public class HellowWorldServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body");
+		out.println("<h1>Hellow bitch</h1>");
+		out.println("</html>");
+		out.println("</body");
+		
+	}
+	
+}
